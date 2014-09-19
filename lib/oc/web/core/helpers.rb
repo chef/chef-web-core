72d33d00b87ceb26acfee50d53b4ec32559b0e87
Christian Nunciato <cnunciato@opscode.com>
2014-09-19 16:30:22 -0700

    Replace KSS with Hologram and Middleman

--- lib/oc/web/core/helpers.rb
+++ lib/oc/web/core/helpers.rb
@@ -1,18 +1,48 @@
 require 'oc/web/core'
+require 'oc/web/core/component'
 
 # Base module for oc-web-core to be used with other frameworks.
 module OC
   module Web
     module Core
       module Helpers
-        # Load a partial from templates/#{name}.html
+
         def oc_partial(name)
           open(File.join(OC::Web::Core::TEMPLATES_PATH, "#{name}.html")).read
         end
 
-        # A title tag
         def oc_title_tag(text = nil)
           "<title>#{text}#{!text.nil? ? ' | ' : ''}Chef</title>"
+        end
+
+        def oc_top_nav(site)
+          OC::Web::Core::Component.new(sites[site].merge(type: :top_nav)).render
+        end
+
+        private 
+
+        # Be nice now, this is just a WIP
+        def sites
+          {
+            corpsite: {
+              name: 'CHEF'
+            },
+            downloads: {
+              name: 'Chef Downloads'
+            },
+            learnchef: {
+              name: 'Learn Chef'
+            },
+            analytics: {
+              name: 'Chef Analytics'
+            },
+            manage: {
+              name: 'Chef Manage'
+            },
+            style: {
+              name: 'CHEF Style'
+            }
+          }
         end
       end
     end
---
+++ lib/oc/web/core/url_helpers.rb
@@ -0,0 +1,19 @@
+require 'oc/web/core'
+
+module OC
+  module Web
+    module Core
+      module URLHelpers
+        
+        def repo_url
+          'https://github.com/opscode/oc-web-core'
+        end
+
+        def repo_issues_url
+          "#{repo_url}/issues"
+        end
+
+      end
+    end
+  end
+end
