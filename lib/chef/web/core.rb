e9ea40b2bdb4201ab5632cdb20f47f12edd87696
Christian Nunciato <cnunciato@opscode.com>
2014-11-21 11:10:00 -0500

    Update for Jordan's feedback

--- lib/chef/web/core.rb
+++ lib/chef/web/core.rb
@@ -3,9 +3,7 @@ require 'chef/web/core/version'
 class Chef
   module Web
     module Core
-      ROOT_PATH = File.expand_path(
-        File.join(File.dirname(__FILE__), '../../..')
-      )
+      ROOT_PATH = File.expand_path('../../..', File.dirname(__FILE__))
       TEMPLATES_PATH = File.join(ROOT_PATH, 'templates')
     end
   end
