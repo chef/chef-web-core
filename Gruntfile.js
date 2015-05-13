ff196c4ebbba6e9c0b8496c45d040ef248f8a740
Christian Nunciato <cnunciato@opscode.com>
2015-05-13 11:08:53 -0700

    Add note, tooltips, refresh method

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,7 +145,7 @@ module.exports = function(grunt) {
         svg: { 
           xmlns: 'http://www.w3.org/2000/svg',
           'xmlns:xlink': 'http://www.w3.org/1999/xlink',
-          viewBox : '0 0 100 100'
+          viewBox: '0 0 100 100'
         }
       },
       default: {
